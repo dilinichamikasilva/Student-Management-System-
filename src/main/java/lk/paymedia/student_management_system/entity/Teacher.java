4bbeb01a6bc4c9231c85b6115a4fce6bfafc7d90
dilini <dilinichamikasilva@gmail.com>
2026-04-15 11:55:32 +0530

    add Teacher entity with relevant fields and relationships for student management system

---
+++ src/main/java/lk/paymedia/student_management_system/entity/Teacher.java
@@ -0,0 +1,40 @@
+package lk.paymedia.student_management_system.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "teachers")
+@Data
+public class Teacher {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String employeeId;
+
+    @Embedded
+    private Name name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Embedded
+    private Address address;
+
+    private String phoneNumber;
+
+    @Column(nullable = false)
+    private String department;
+
+    private String specialization;
+
+    private String about;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+
+
+}
