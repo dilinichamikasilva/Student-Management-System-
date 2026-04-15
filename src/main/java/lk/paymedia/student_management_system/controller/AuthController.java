d2176e9e8ad0837bb5ecc381faf67fd4fd58efcd
dilini <dilinichamikasilva@gmail.com>
2026-04-15 13:22:42 +0530

    add API response utility and update AuthController with request mapping and logging

--- src/main/java/lk/paymedia/student_management_system/controller/AuthController.java
+++ src/main/java/lk/paymedia/student_management_system/controller/AuthController.java
@@ -1,8 +1,20 @@
 package lk.paymedia.student_management_system.controller;
 
+import lk.paymedia.student_management_system.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/v1")
+@RequiredArgsConstructor
+@Slf4j
 public class AuthController {
+    private final AuthService authService;
+
+
+
+
 
 }
---
+++ src/main/java/lk/paymedia/student_management_system/util/APIResponse.java
@@ -0,0 +1,14 @@
+package lk.paymedia.student_management_system.util;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class APIResponse {
+    private int status;
+    private String message;
+    private Object data;
+}
