b98451ae5556fed73c894744b15e5622abb05826
dilini <dilinichamikasilva@gmail.com>
2026-04-15 11:05:04 +0530

    add Role, RoleType, and User entities for student management system

---
+++ src/main/java/lk/paymedia/student_management_system/entity/Role.java
@@ -0,0 +1,17 @@
+package lk.paymedia.student_management_system.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "roles")
+@Data
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(unique = true, nullable = false)
+    private RoleType role;
+}
---
+++ src/main/java/lk/paymedia/student_management_system/entity/RoleType.java
@@ -0,0 +1,7 @@
+package lk.paymedia.student_management_system.entity;
+
+public enum RoleType {
+    ROLE_STUDENT,
+    ROLE_TEACHER,
+    ROLE_ADMIN
+}
